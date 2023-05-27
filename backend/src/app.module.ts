aa752beb556f0df38ae76cf07184a69ef869761d
Nam Phan <namph.tech@gmail.com>
2023-05-27 11:39:44 +0000

    ✨ Connect BE with DB container

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,31 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SocketGatewayModule } from './socket-gateway/socket-gateway.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+let envFilePath = '../.env.development';
+if (process.env.ENVIRONMENT === 'PRODUCTION') {
+  envFilePath = '../.env.production';
+}
 
 @Module({
-  imports: [SocketGatewayModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    SocketGatewayModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      synchronize: false,
+      entities: [],
+      logging: process.env.MYSQL_LOGGING ? true : false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
