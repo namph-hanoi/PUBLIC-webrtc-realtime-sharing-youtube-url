bde8d84f561ea9759b61d4493126ffd746504944
Nam Phan <namph.tech@gmail.com>
2023-06-03 18:01:04 +0700

    ✨ Install Cypress for e2e testing

---
+++ frontend/cypress/e2e/sharing-spec.ts
@@ -0,0 +1,8 @@
+describe('template spec', () => {
+  it('Fail', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  it('Success', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
