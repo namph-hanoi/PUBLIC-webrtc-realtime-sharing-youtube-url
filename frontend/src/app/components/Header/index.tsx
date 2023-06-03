52bb015cdb794a3b36d97ea912a755a19941136d
Nam Phan <namph.tech@gmail.com>
2023-06-03 09:09:24 +0000

    ✨FE: install react-hook-form with yup

--- frontend/src/app/components/Header/index.tsx
+++ frontend/src/app/components/Header/index.tsx
@@ -1,14 +1,56 @@
 import React from 'react';
-import { Container } from '@mui/material';
+import { Checkbox, Container, TextField } from '@mui/material';
+
 import 'tailwindcss/tailwind.css'
+import { Controller, useForm } from 'react-hook-form';
 
 
 export default function Header() {
+  const { handleSubmit, control } = useForm();
+  
+
   return (
-    <div>
-      <p className='text-6xl font-bold underline'>Alo</p>
-      <Container>
-        This is the header
+    <div className='pt-5'>
+      <Container className='text-right'>
+        <form onSubmit={handleSubmit(data => console.log(data))}>
+          <span className='inline-block'>
+            <Controller
+              control={control}
+              name="email"
+              render={({
+                field: { onChange, onBlur, value, name, ref },
+                fieldState: { invalid, isTouched, isDirty, error },
+                formState,
+              }) => (
+                <TextField
+                  onChange={onChange}
+                  error={!!error}
+                  label="Email"
+                  value={value}
+                />
+              )}
+            />
+          </span>
+          <span className='inline-block ml-2'>
+            <Controller
+              control={control}
+              name="password"
+              render={({
+                field: { onChange, onBlur, value, name, ref },
+                fieldState: { invalid, isTouched, isDirty, error },
+                formState,
+              }) => (
+                <TextField
+                  onChange={onChange}
+                  error={!!error}
+                  label="Password"
+                  value={value}
+                  type='password'
+                />
+              )}
+            />
+          </span>
+        </form>
       </Container>
     </div>
   )
