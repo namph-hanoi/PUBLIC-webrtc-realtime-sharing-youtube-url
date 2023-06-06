0486c39d12dbb46b614dc5467a11b7e24253586e
Nam Phan <namph.tech@gmail.com>
2023-06-06 16:34:50 +0000

    ✨ FE: connect socket and listen on newSharing event

--- frontend/src/app/containers/Global/index.tsx
+++ frontend/src/app/containers/Global/index.tsx
@@ -1,12 +1,33 @@
+import React from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import LoadingBox from '../../components/LoadingBox';
 import { ReactNode } from 'react';
 import { useAppSelector } from '../../hooks';
 import { selectGlobalState } from './globalSlice';
+import * as socketIOClient from "socket.io-client";
+import { Socket } from 'socket.io-client';
+
 
 export default function GlobalWrap({ children }: {children: ReactNode}) {
-  const { globalLoading } = useAppSelector(selectGlobalState);
+  const { userEmail, globalLoading } = useAppSelector(selectGlobalState);
+
+  const socketRef = React.useRef<Socket | undefined>();
+
+  React.useEffect(() => {
+    if (userEmail) {
+      socketRef.current = socketIOClient.connect(`${window.location.origin}`, {
+        extraHeaders: {
+          Authorization: `Bearer ${localStorage.getItem('ACCESS_TOKEN_KEY')}`
+        }
+      });
+
+      socketRef.current.on('newSharing', (data) => {
+        console.log(["🚀 ~ file: index.tsx:26 ~ React.useEffect ~ data:", data]);
+      })
+    }
+    
+  }, [userEmail]);
 
   return (
     <>
